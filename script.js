4b99eb44cfeaaf6040844b0e72055c74f8dbe301
sadikul41 <112966434+sadikul41@users.noreply.github.com>
2022-09-06 22:02:19 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var day = prompt();
+
+if(day === "saterday") {
+    document.write("Today is Workday");
+}else if(day === "sunday") {
+    document.write("Today is workday of sunday");
+}else if(day === "monday") {
+    document.write("this is day of monday");
+}else if(day === "twesday"){
+    document.write("This is a day of Twesday");
+}else if(day === "wednesday"){
+    document.write("This is another day of Wednesday");
+}else if(day === "thusday"){
+    document.write("This is Half office day of Thusday");
+}else if(day ==="friday"){
+    document.write("Today is holiday bcause of Friday");
+}
+else {
+    document.write("you are not typing a day");
+}
